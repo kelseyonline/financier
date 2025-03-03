80c1224534d81c8f9e5230e77d94c29f5e4a2e72
Kelsey Dabrowski <kdabrowski103@gmail.com>
2025-03-02 21:17:54 -0500

    Hook up CSS to file and make result visible only at end of sequence

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ nextButton.addEventListener('click', function() {
     } else if (clickCount === 2) {
         document.getElementById('message').innerText = 'How much money did you just spend?';
         document.getElementById('nextButton').innerText = 'Shame me';
+    } else if (clickCount === 3) {
+        var resultMessage = document.getElementById('resultMessage');
+        resultMessage.style.display = 'block';
         clickCount = 0; 
     }
 });
